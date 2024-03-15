835c0691ea34dcfff46a311ffe8a5f3c25ae763c
hhh <hhh@-1>
2024-03-15 10:52:23 +0300

    Добавьте файлы проекта.

---
+++ hhh3.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a = 5;
+    int b = 10;
+
+    // сума і різниця
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    cout << "Метод 1: a = " << a << ", b = " << b << endl;
+
+    // множення і ділення
+    a = 5;
+    b = 10;
+    a = a * b;
+    b = a / b;
+    a = a / b;
+    cout << "Метод 2: a = " << a << ", b = " << b << endl;
+
+    // використання виразу присвоювання
+    a = 5;
+    b = 10;
+    a = (a + b) - (b = a);
+    cout << "Метод 3: a = " << a << ", b = " << b << endl;
+
+    return 0;
+}
